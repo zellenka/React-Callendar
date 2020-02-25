5682314adf5915c997efd2468a696acf8eaba164
Serhii <seleshhenko@yahoo.com>
2020-02-25 15:32:37 +0200

    docs folder

---
+++ docs/precache-manifest.99e14bb0ac2a2ba04791f6abef2e98dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220f674ed1ef42b8c44451030953b86e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac01c6c08c339c3aed1c",
+    "url": "./static/css/2.96a082ff.chunk.css"
+  },
+  {
+    "revision": "580a62b3665ff5ca8bb5",
+    "url": "./static/css/main.36e16804.chunk.css"
+  },
+  {
+    "revision": "ac01c6c08c339c3aed1c",
+    "url": "./static/js/2.58178bbc.chunk.js"
+  },
+  {
+    "revision": "0a933658d5a9affb81d89e61ee299db6",
+    "url": "./static/js/2.58178bbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "580a62b3665ff5ca8bb5",
+    "url": "./static/js/main.7448407f.chunk.js"
+  },
+  {
+    "revision": "9c3059981fecc600ce7c",
+    "url": "./static/js/runtime-main.79423d96.js"
+  }
+]);
